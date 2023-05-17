460e7b5fa5ea23a730f165ab67be09731164ca33
edensalt <124738007+edensalt@users.noreply.github.com>
2023-05-17 11:33:58 -0500

    Remove to do list from dom

--- src/modules/remove_list_item.js
+++ src/modules/remove_list_item.js
@@ -1,5 +1,20 @@
+import RenderToDoList from '../components/list';
+import { masterList } from './create_list_item';
+
+function resetList() {
+  const listContainer = document.querySelector('#list-container');
+  while (listContainer.firstChild) {
+    listContainer.removeChild(listContainer.firstChild);
+  }
+}
+
 const removeItem = function (e) {
-  console.log(e.target); // target index
+  console.log(e.target.getAttribute('index'));
+  masterList.splice(e.target.getAttribute('index'), 1);
+  console.table(masterList);
+  resetList();
+  RenderToDoList();
+  return masterList;
 };
 
 export default removeItem;
