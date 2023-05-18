88c1eceed4101c3582078ece577469f26b674ac2
edensalt <124738007+edensalt@users.noreply.github.com>
2023-05-18 16:06:04 -0500

    Remove warning of deleting projects

--- src/modules/remove_project.js
+++ src/modules/remove_project.js
@@ -1,17 +1,52 @@
 import { masterProjectList } from './create_project';
 import { masterList } from './create_list_item';
+import HomePage from '../pages/homepage';
 
 // Add warning about removing projects
 
+function removeProjectWarning(project) {
+  const parent = document.querySelector('#parent');
+  const popup = document.createElement('div');
+  popup.setAttribute('id', 'warning-popup');
+  popup.classList.add('fixed', 't-2', 'l-2');
+
+  const para = document.createElement('p');
+  para.innerHTML = 'Confirm project deletion. If you delete this project, all tasks associated with the project will be deleted. Please ensure you have reassigned these tasks. Okay to proceed?';
+
+  const cancelBtn = document.createElement('button');
+  cancelBtn.innerHTML = 'Cancel';
+  cancelBtn.classList.add('btn-complete');
+
+  const confirmBtn = document.createElement('button');
+  confirmBtn.innerHTML = 'Delete project';
+  confirmBtn.classList.add('btn-complete');
+
+  parent.appendChild(popup);
+  popup.appendChild(para);
+  para.appendChild(cancelBtn);
+  para.appendChild(confirmBtn);
+
+  cancelBtn.addEventListener('click', () => popup.remove());
+
+  confirmBtn.addEventListener('click', () => {
+    for (let i = masterList.length - 1; i >= 0; i--) {
+      if (masterList[i].project === masterProjectList[project].project) {
+        masterList.splice(i, 1);
+      }
+    }
+    masterProjectList.splice(project, 1);
+    popup.remove();
+    const main = document.querySelector('#main');
+    main.innerHTML = '';
+    HomePage();
+  });
+}
+
+// const confirmRemoveProject
+
 const removeProject = function (e) {
   const project = e.target.getAttribute('index');
-  for (let i = masterList.length - 1; i >= 0; i--) {
-    if (masterList[i].project === masterProjectList[project].project) {
-      masterList.splice(i, 1);
-    }
-  }
-  masterProjectList.splice(e.target.getAttribute('index'), 1);
-  return masterProjectList;
+  removeProjectWarning(project);
 };
 
 export default removeProject;
