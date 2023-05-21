22c2fc8aac54fff75a4e53f95ec3241646a3bf9a
edensalt <124738007+edensalt@users.noreply.github.com>
2023-05-21 16:49:48 -0500

    Add filters to dom

--- src/components/list_filters.js
+++ src/components/list_filters.js
@@ -1,15 +1,17 @@
-import addFilter from './list_fiters_components';
+import { addCompleteFilter, addAlphaFilter, addNumFilter } from './list_fiters_components';
 
 const RenderListFilters = function () {
   const parent = document.querySelector('#items');
 
   const container = document.createElement('div');
-  container.innerHTML = 'hi';
   container.setAttribute('id', 'list-filters');
   container.classList.add('py-2', 'bg-rose-200', 'grid', 'grid-cols-list', 'gap-2', 'items-center', 'text-sm', 'px-8');
-  container.appendChild(addFilter('Title', 'title'));
-  container.appendChild(addFilter('Description', 'description'));
-  container.appendChild(addFilter('Project', 'project'));
+  container.appendChild(addCompleteFilter('status'));
+  container.appendChild(addAlphaFilter('Title', 'title'));
+  container.appendChild(addAlphaFilter('Description', 'description'));
+  container.appendChild(addAlphaFilter('Project', 'project'));
+  container.appendChild(addNumFilter('priority'));
+  container.appendChild(addAlphaFilter('Date', 'date'));
 
   parent.appendChild(container);
 
